7c19319afd3e4c23c26b2253b69328c5e040b90c
ZainubKarim <139782177+ZainubKarim@users.noreply.github.com>
2023-09-23 21:00:14 +0500

    Add files via upload

---
+++ exercise#19/index.js
@@ -0,0 +1,5 @@
+"use strict";
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+Object.defineProperty(exports, "__esModule", { value: true });
+const Exercise_14_1 = require("../Exercise#14");
+console.log(`We are inviting ${Exercise_14_1.guestlist.length} guests to the dinner.`);
---
+++ exercise#19/index.ts
@@ -0,0 +1,6 @@
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+import{ guestlist } from '../Exercise#14'
+
+console.log(`We are inviting ${guestlist.length} guests to the dinner.`);
+
