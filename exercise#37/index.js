3476a74f060c4b10362df22fa2e2ce7b3d89b1fb
ZainubKarim <139782177+ZainubKarim@users.noreply.github.com>
2023-09-23 21:31:24 +0500

    Add files via upload

---
+++ exercise#37/index.js
@@ -0,0 +1,11 @@
+"use strict";
+// Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message that reads I love TypeScript. 
+// Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
+function make_shirt(size = "Large", message = "I love TypeScript.") {
+    console.log(`Size: ${size}, Message: ${message}`);
+}
+make_shirt();
+// Default msg for medium
+make_shirt("Medium");
+// Default msg for small
+make_shirt("Small", "Hello World!");
