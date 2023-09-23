c15b1bd38bd87678a0d13be1d5f513c7ba972086
ZainubKarim <139782177+ZainubKarim@users.noreply.github.com>
2023-09-23 21:30:22 +0500

    Add files via upload

---
+++ exercise#36/index.ts
@@ -0,0 +1,14 @@
+// T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. 
+// The function should print a sentence summarizing the size of the shirt and the message printed on it. 
+
+
+function make_shirt(size: string, message: string): void {
+    console.log(`The size of the shirt is ${size} and the message is ${message}`);
+};
+
+// Call the function
+
+make_shirt("S", "Cute person above.");
+make_shirt("M", "Cute person above.");
+make_shirt("L", "Cute person above.");
+
