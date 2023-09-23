9c409030410d144f1b089bce06296febcc9e38e5
ZainubKarim <139782177+ZainubKarim@users.noreply.github.com>
2023-09-23 20:42:33 +0500

    Add files via upload

---
+++ exercise#10/index.ts
@@ -0,0 +1,20 @@
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+// I added comment on the program of exercise no. 3 
+
+/* let personName: string = "zainub karim";
+
+
+let lowercaseName: string = personName.toLowerCase();
+console.log(lowercaseName);
+
+
+let uppercaseName: string = personName.toUpperCase();
+console.log(uppercaseName);
+
+
+let words: string[] = personName.split(" ");
+let titlecaseName: string = ""
+for (let i = 0 ; i < words.length; i++) {
+    titlecaseName +=words[i].charAt(0).toUpperCase() +words[i].slice(1).toLowerCase()+" "
+};
+console.log(titlecaseName); */
