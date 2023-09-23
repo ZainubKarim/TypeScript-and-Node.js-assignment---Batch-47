132b802d5e3a2a22d555e707438bd9018db68a12
ZainubKarim <139782177+ZainubKarim@users.noreply.github.com>
2023-09-23 20:51:47 +0500

    Add files via upload

---
+++ exercise#14/index.js
@@ -0,0 +1,15 @@
+"use strict";
+/* Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite?
+Make a list that includes at least three people you’d like to invite to dinner.
+Then use your list to print a message to each person, inviting them to dinner */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.guestlist = void 0;
+let guestlist = [
+    "Zainub Karim",
+    "Ayesha Karim",
+    "Saad Waseem",
+    "Kanwal Karim",
+    "Amman Messay"
+];
+exports.guestlist = guestlist;
+console.log("🚀 ~ file: index.ts:13 ~ guestlist:", guestlist);
