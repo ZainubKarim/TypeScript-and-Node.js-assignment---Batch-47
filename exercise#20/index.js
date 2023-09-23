bbe87af91e41e645e2d7547e494655174f02bf08
ZainubKarim <139782177+ZainubKarim@users.noreply.github.com>
2023-09-23 21:02:27 +0500

    Add files via upload

---
+++ exercise#20/index.js
@@ -0,0 +1,10 @@
+"use strict";
+/* Think of something you could store in a array.
+ For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like.
+ Write a program that creates a list containing these items. */
+const languages = ["Spanish", "Urdu", "English", "Punjabi"];
+console.log("List of Languages:");
+for (let language of languages) {
+    console.log(language);
+}
+;
