9418056e3b57e759652224ad5de27a70ff838205
ZainubKarim <139782177+ZainubKarim@users.noreply.github.com>
2023-09-23 20:30:49 +0500

    Add files via upload

---
+++ exercise#5/index.js
@@ -0,0 +1,5 @@
+"use strict";
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+let famous_person = "Louisa May";
+let message = famous_person + ' once said, “I am not afraid of storms, for I am learning how to sail my ship.”';
+console.log(message);
